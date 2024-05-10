18a26b04494f7db6798cfcabe41418c671c85e5e
Dheena018 <deenatamilnathan007@gmail.com>
2024-05-10 18:32:14 +0530

    i did menu bar component

--- app/components/menuBar.tsx
+++ app/components/menuBar.tsx
@@ -14,7 +14,7 @@ const MenuBar: React.FC<MenuBarProps> = ({ menuBar, setMenuBar }) => {
                 <section className="bg-white">
                     <div className="container mx-auto"> 
                         <div className='w-full'>
-                            <ul className=''>{NavInfo.map((item, index) => <li key={index} className=' border border-gray-100 hover:border hover:border-dotted hover:border-black px-4 py-4'><a href="" className='text-medium'>{item}</a></li>)}</ul>
+                            <ul className=''>{NavInfo.map((item, index) => <li key={index} className=' border border-gray-50 hover:border hover:border-dotted hover:border-black px-4 py-4'><a href="" className='text-medium font-semibold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>{item}</a></li>)}</ul>
                         </div>
                     </div>
                 </section>
